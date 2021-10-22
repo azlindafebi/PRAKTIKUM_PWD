86cc15a872c892203cba229f733808178fd52cd0
azlindafebi <62742248+azlindafebi@users.noreply.github.com>
2021-10-22 23:29:03 +0700

    kegiatan praktikum 3

--- PRAKTIKUM3/koneksi.php
+++ PRAKTIKUM3/koneksi.php
@@ -11,16 +11,4 @@ if (!$con) {
     exit();
 }
 
-<?php
-$host           = "localhost";
-$username       = "root";
-$password       = "";
-$databasename   ="akademik";
-$con            = @mysqli_connect($host,$username,$password,$databasename);
-
-if (!$con) {
-    echo "Error: ". mysqli_connect_error();
-    exit();
-}
-
 ?>
